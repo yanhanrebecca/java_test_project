141569e3e6d3a411a705d12e36e5e00b6cecaaa2
rebeccayanhan <2628360427@qq.com>
2023-03-25 21:57:46 +0800

    TestEqual

---
+++ src/string/TestEqual.java
@@ -0,0 +1,31 @@
+package string;
+
+public class TestEqual {
+    public static void main(String[] args){
+        //字符串比较的集中对比
+        String s1 = new String("abc");
+        String s2 = new String("ABC");
+        String s3 = new String("abc");
+        //int address1 = System.identityHashCode(s1);
+        //System.out.println(address1);
+        boolean b2 = s1.equalsIgnoreCase(s2);
+        boolean b = s1.equals(s2);
+        System.out.println(s1 + " equal " + s2 + " :" + b);
+        System.out.println(s1 + " equalsIgnoreCase " + s2 + " :" + b2);
+        b = (s1 == s3);
+        System.out.println("s1 == s3 " + b);
+        String s4 = "abc";
+        String s5 = "abc";
+        String s6 = "abcd";
+        b = (s4 == s5);
+        System.out.println("s4 == s5 " + b);
+        b = (s5 == s6);
+        System.out.println("s5 == s6 " + b);
+        s6 = "abc";
+        b = (s5 == s6);
+        System.out.println("*********************s5==s6 " + b);
+        s6 = "abc";
+        String  str = "abcd";
+        System.out.println(str.compareTo("cat"));
+    }
+}
