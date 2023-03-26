d3299752f39b089d457f8fde181baa5156d0c5a9
rebeccayanhan <2628360427@qq.com>
2023-03-26 21:43:42 +0800

    judge_e_mail

---
+++ src/string/judge_e_mail.java
@@ -0,0 +1,20 @@
+package string;
+
+public class judge_e_mail {
+    public static void main(String [] args){
+        String regex = "\\w+@\\w+(\\.\\w{2,3})*\\.\\w{2,3}";
+        String str1 = "aaa@";
+        String str2 = "aaaaaaa";
+        String str3 = "1111@1111ffyu.dfg.com";
+        //matches用于判断字符串是否和正则表达式匹配
+        if (str1.matches(regex)) {
+            System.out.println(str1 + "是一个合法的E_mail地址");
+        }
+        if (str2.matches(regex)) {
+            System.out.println(str2 + "是一个合法的E_mail地址");
+        }
+        if (str3.matches(regex)) {
+            System.out.println(str3 + "是一个合法的E_mail地址");
+        }
+    }
+}
