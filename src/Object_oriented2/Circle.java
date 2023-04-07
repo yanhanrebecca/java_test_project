7131eb7a7d34036a92ab32fd8a12ffd383c37f19
rebeccayanhan <2628360427@qq.com>
2023-04-07 11:12:25 +0800

    shape

--- src/Object_oriented2/Circle.java
+++ src/Object_oriented2/Circle.java
@@ -1,2 +1,19 @@
-package Object_oriented2;public class Circle {
+package Object_oriented2;
+
+public class Circle extends Shape{
+    double r;
+    final double PI = 3.1415;
+    public Circle(double r){
+        this.r = r;
+    }
+    public Circle(){};
+    public double getGirth(){
+        girth = 2 * PI * r;
+        return girth;
+    }
+    public double getArea(){
+        area = PI * r * r;
+        return area;
+    }
+
 }
--- src/Object_oriented2/Shape.java
+++ src/Object_oriented2/Shape.java
@@ -1,2 +1,12 @@
-package Object_oriented2;public class Shape {
+package Object_oriented2;
+
+public class Shape {
+    double girth;//周长
+    double area;//面积
+    public double Shape(){
+        return girth;
+    }
+    public double getArea(){
+        return area;
+    }
 }
