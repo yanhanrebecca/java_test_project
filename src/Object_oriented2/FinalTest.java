1843631c2c69fa59ff89ff35b679e7d2c0ed6bec
rebeccayanhan <2628360427@qq.com>
2023-04-07 12:08:50 +0800

    push

---
+++ src/Object_oriented2/FinalTest.java
@@ -0,0 +1,19 @@
+package Object_oriented2;
+// 终结类：不能被继承
+public final class FinalTest {
+
+    // 常量：只能通过显示初始化或构造器初始化
+    public final int x = 1;
+
+    // 终结方法：不能被 重写
+    public final void method() {
+        // 局部常量
+        final int y = 2;
+    }
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
