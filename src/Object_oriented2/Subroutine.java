8d90c10a6263f04c27521a5b8078b02b50ab8f39
rebeccayanhan <2628360427@qq.com>
2023-04-07 11:50:54 +0800

    parent

---
+++ src/Object_oriented2/Subroutine.java
@@ -0,0 +1,19 @@
+package Object_oriented2;
+class Parent {
+    Parent(){
+        System.out.println("调用父类的parent()构造方法");
+    }
+}
+class SubParent extends  Parent{
+    SubParent(){
+        System.out.println("调用子类的Subparent()构造方法");
+    }
+}
+public class Subroutine extends SubParent{
+    Subroutine(){
+        System.out.println("调用子类的Subroutine()构造方法");
+    }
+    public static void main(String [] args){
+        Subroutine s = new Subroutine();
+    }
+}
