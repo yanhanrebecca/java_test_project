22448ea8402e2af89f719798dddf97d2ef3f8803
rebeccayanhan <2628360427@qq.com>
2023-04-27 13:39:03 +0800

    QuotientWithMethod

---
+++ src/exception/QuotientWithMethod.java
@@ -0,0 +1,18 @@
+package exception;
+import java.util.Scanner;
+
+public class QuotientWithMethod {
+    public static int quotient(int n1, int n2){
+        return n1/n2;
+    }
+    public static void main(String[] args){
+        Scanner r = new Scanner((System.in));
+        System.out.println("Enter two Integers:");
+        int n1 = r.nextInt();
+        int n2 = r.nextInt();
+        r.close();
+        int result = quotient(n1, n2);
+        System.out.println(n1 + " / " + n2 + " is " + result);
+        System.out.println("Execution continues...");
+    }
+}
