7480ecc0aa56a86d8c1b89081cb593b7826111cb
rebeccayanhan <2628360427@qq.com>
2023-05-21 22:31:52 +0800

    作业

--- src/file_homework.java
+++ src/file_homework.java
@@ -1,14 +1,17 @@
-//D:\\sample2023.txt    （每行代表一个文件，可能有很多行，大家统计文件类型个数，并把统计得到的文件类型输出出来）
-//        5月25日上交纸质版，写上学号姓名。
-//
-//        2018/10/12  17:11           189,137    20181005-01_页面_1.jpg
-//        2018/10/10  16:19           137,564 20181005-01.pdf
-//        2018/10/12  17:11           189,124    20181005-01_页面_2.jpg
-//        2013/04/23  09:25           1,940,285  00-12.pdf
-//        2018/10/12  15:49            12,022 wwww.docx
+/*
+D:\\sample2023.txt    （每行代表一个文件，可能有很多行，大家统计文件类型个数，并把统计得到的文件类型输出出来）
+        5月25日上交纸质版，写上学号姓名。
+
+        2018/10/12  17:11           189,137    20181005-01_页面_1.jpg
+        2018/10/10  16:19           137,564 20181005-01.pdf
+        2018/10/12  17:11           189,124    20181005-01_页面_2.jpg
+        2013/04/23  09:25           1,940,285  00-12.pdf
+        2018/10/12  15:49            12,022 wwww.docx
+*/
 import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Set;
 
 public class file_homework {
     public static void main(String [] args) throws IOException {
@@ -42,6 +45,11 @@ public class file_homework {
 
             }
         }
-    System.out.println(result);
+        Set<String> keys = result.keySet();
+        int key_length = result.keySet().size();
+        System.out.println("经统计，文件类型一共有 " + key_length + " 种");
+        for (Object key : keys) {
+            System.out.println(key + "类型的文件共有" + result.get(key) + "个");
+        }
     }
 }
