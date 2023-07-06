d3237af5b0d64981d4911b7e6b864dc169a2db2d
rebeccayanhan <2628360427@qq.com>
2023-07-06 15:36:26 +0800

    exam

--- src/examination/Eception_Demo.java
+++ src/examination/Eception_Demo.java
@@ -10,17 +10,19 @@ class MyException extends Exception{
 }
 public class Eception_Demo {
     static void compute(int a) throws MyException{
-        System.out.println("Called compute("+a+")");
+        System.out.println("Called compute[#"+a+"#]");
         if(a > 20)throw new MyException(a);
         System.out.println("Normal exit!");
     }
     public static void main(String [] args){
         try{
-            compute(20);
-            compute(33);
-            compute(1);
+            compute(15);
+            compute(21);
+            compute(7);
         }catch (MyException e){
             System.out.println("Exceptiob caught :" + e.toString());
+        }finally {
+            System.out.println("Finally");
         }
     }
 }
